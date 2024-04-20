b3ae1248a172828497f697d3fbe1acbc9283ece8
nileshzarkar <nileshzarkar@gmail.com>
2024-04-20 17:39:50 +0530

    added data structure

---
+++ 2-datastructures.py
@@ -0,0 +1,29 @@
+# list example in python
+numbers = [1, 2, 3, 4, 5]
+print(type(numbers))
+print(numbers)
+
+
+# tuple example
+favorite_colors = ("blue", "green", "red")
+print(type(favorite_colors))
+print(favorite_colors[0])
+print(favorite_colors[1])
+print(favorite_colors[2])
+
+
+# dictionaries example in python
+person = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+print(type(person))
+print(person["name"])
+print(person["age"])
+print(person["city"])
+
+# sets example in python
+my_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(type(my_set))
+print(my_set)
